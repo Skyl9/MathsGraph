965aea5e9ebba6963523881d601d8d54893aa61d
Skyl9 <tristan.rigaud@nrmi.fr>
2025-03-31 21:25:13 +0200

    Mise à jour pour accepter le .env et les variables d'environnements

--- database.py
+++ database.py
@@ -1,15 +1,20 @@
+import os
 import psycopg2
-import configparser
+from dotenv import load_dotenv
 
-# Charger le fichier de configuration
-config = configparser.ConfigParser()
-config.read("config.ini")
+# Charger les variables d'environnement depuis .env
+load_dotenv()
+
 def get_db_connection():
-    db_config = config["database"]
-    return psycopg2.connect(
-        dbname=db_config["DB_NAME"],
-        user=db_config["DB_USER"],
-        password=db_config["DB_PASSWORD"],
-        host=db_config["DB_HOST"],
-        port=db_config.getint("PORT")
-    )
+    try:
+        conn = psycopg2.connect(
+            dbname=os.getenv("DB_NAME"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            host=os.getenv("DB_HOST"),
+            port=os.getenv("DB_PORT")
+        )
+        return conn
+    except psycopg2.Error as e:
+        print(f"❌ Erreur de connexion à PostgreSQL : {e}")
+        return None  # Permet d'éviter les plantages si la connexion échoue
