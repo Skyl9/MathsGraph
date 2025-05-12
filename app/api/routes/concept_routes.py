9bc78a65d2e55518aeb41a021063fba8bfaa16cd
Skyl9 <tristan.rigaud@nrmi.fr>
2025-05-12 14:39:02 +0200

    Refactor concept retrieval to use ConceptService
    Moved concept-related logic from the routes to the ConceptService for better separation of concerns and maintainability. This change ensures cleaner routes and centralizes the business logic in the service layer.

--- app/api/routes/concept_routes.py
+++ app/api/routes/concept_routes.py
@@ -17,126 +17,6 @@ from app.services.concept_service import ConceptService
 
 router = APIRouter(prefix="", tags=["concepts"])
 
-
-def get_mathematiciens(conn) -> MathematicienResponse:
-    with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
-        cur.execute("SELECT * FROM mathematiciens")
-        return cur.fetchall()
-
-
-def get_categories(conn) -> CategorieBase:
-    with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
-        cur.execute("SELECT * FROM categories")
-        return cur.fetchall()
-
-
-def get_concept_info(concept_id, connection) -> ConceptResponse:
-    with connection.cursor() as cursor:
-        # Récupérer les informations de base sur le concept
-        cursor.execute("""
-                       SELECT c.id,
-                              c.nom,
-                              t.type,
-                              c.enonce,
-                              c.demonstration,
-                              c.verification,
-                              c.date_modification,
-                              m.id,
-                              m.nom,
-                              cat.id,
-                              cat.nom
-                       FROM concepts c
-                                LEFT JOIN mathematiciens m ON c.mathematicien_id = m.id
-                                LEFT JOIN categories cat ON c.categorie_id = cat.id
-                                LEFT JOIN type t ON c.type_id = t.id
-                       WHERE c.id = %s
-                       ORDER BY c.id ASC
-                       """, (concept_id,))
-        result = cursor.fetchone()
-
-        if not result:
-            return None  # Si le concept n'existe pas
-
-        concept = {
-            "id": result[0],
-            "nom": result[1],
-            "type": result[2],
-            "enonce": result[3],
-            "demonstration": result[4],
-            "verification": result[5],
-            "date_modification": result[6],
-            "mathematicien": {"id": result[7], "mathematicien": result[8]}
-            if result[7] else None,
-            "categorie": {"id": result[9], "category": result[10]}
-            if result[9] else None,
-        }
-        print(concept)
-
-        # Récupérer les alias du concept
-        cursor.execute("SELECT alias FROM aliases WHERE concept_id = %s", (concept_id,))
-        concept["aliases"] = [row[0] for row in cursor.fetchall()]
-
-        # Récupérer les sources liées au concept
-        cursor.execute("""
-                       SELECT DISTINCT s.id, s.titre, s.auteur, s.annee, s.url, s."type"
-                       FROM sources s
-                                JOIN concepts_sources cs ON s.id = cs.source_id
-                       WHERE cs.concept_id = %s
-                       """, (concept_id,))
-        concept["sources"] = [
-            {
-                "id": row[0],
-                "titre": row[1],
-                "auteur": row[2],
-                "annee": row[3],
-                "url": row[4],
-                "type": row[5],
-            } for row in cursor.fetchall()
-        ]
-
-        # Récupérer les relations du concept (sources ou cibles)
-        cursor.execute("""
-                       SELECT r.id,
-                              r.concept_source,
-                              c_source.nom AS nom_source,
-                              r.concept_cible,
-                              c_cible.nom  AS nom_cible,
-                              r.type_relation,
-                              r.description,
-                              r.date_relation
-                       FROM relations r
-                                JOIN concepts c_source ON r.concept_source = c_source.id
-                                JOIN concepts c_cible ON r.concept_cible = c_cible.id
-                       WHERE concept_source = %s
-                          OR concept_cible = %s
-                       """, (concept_id, concept_id))
-        concept["relations"] = [
-            {
-                "id": row[0],
-                "concept_source": {"id": row[1], "nom": row[2]},
-                "concept_cible": {"id": row[3], "nom": row[4]},
-                "type_relation": row[5],
-                "description": row[6],
-            } for row in cursor.fetchall()
-        ]
-        cursor.execute("""
-                       SELECT id, "Nom_francais", "Nom_étranger", langue
-                       FROM foreign_name
-                       WHERE "Nom_francais" = (SELECT nom FROM concepts WHERE id = %s)
-
-                       """, (concept_id,))
-        concept["noms_etrangers"] = [
-            {
-                "id": row[0],
-                "Nom_francais": row[1],
-                "Nom_étranger": row[2],
-                "langue": row[3],
-            } for row in cursor.fetchall()
-        ]
-
-        return concept
-
-
 def get_conceptsAdmin(conn) -> List[ConceptResponse]:
     with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
         # Récupérer les concepts
@@ -210,9 +90,6 @@ def get_conceptsAdmin(conn) -> List[ConceptResponse]:
 
         return result_concepts
 
-
-
-
 @router.get("/getAlldatabaseInfo", response_model=List[ConceptResponse])
 def giveAllDatabaseInfo():
     conn = get_db_connection()
@@ -220,12 +97,8 @@ def giveAllDatabaseInfo():
 
 
 @router.get("/concept/{concept_id}", response_model=ConceptResponse)
-def getNode(concept_id: int):
-    conn = get_db_connection()
-    concept = get_concept_info(concept_id, conn)
-    if not concept:
-        raise HTTPException(status_code=404, detail="Concept non trouvé")
-    return concept
+def getConcept(concept_id: int):
+    return ConceptService.get_concept_info(concept_id)
 
 
 @router.get("/getEditableFieldsOptions",response_model=EditableField)
--- app/services/concept_service.py
+++ app/services/concept_service.py
@@ -3,9 +3,117 @@ from fastapi import HTTPException
 from app.db.database import get_db_connection
 from app.schemas import UpdateConceptDict
 
-from app.schemas.concept import ConceptName
+from app.schemas.concept import ConceptName, ConceptResponse
+
 
 class ConceptService:
+    @staticmethod
+    def get_concept_info(concept_id) -> ConceptResponse:
+        connection = get_db_connection()
+        with connection.cursor() as cursor:
+            # Récupérer les informations de base sur le concept
+            cursor.execute("""
+                           SELECT c.id,
+                                  c.nom,
+                                  t.type,
+                                  c.enonce,
+                                  c.demonstration,
+                                  c.verification,
+                                  c.date_modification,
+                                  m.id,
+                                  m.nom,
+                                  cat.id,
+                                  cat.nom
+                           FROM concepts c
+                                    LEFT JOIN mathematiciens m ON c.mathematicien_id = m.id
+                                    LEFT JOIN categories cat ON c.categorie_id = cat.id
+                                    LEFT JOIN type t ON c.type_id = t.id
+                           WHERE c.id = %s
+                           ORDER BY c.id ASC
+                           """, (concept_id,))
+            result = cursor.fetchone()
+
+            if not result:
+                return None  # Si le concept n'existe pas
+
+            concept = {
+                "id": result[0],
+                "nom": result[1],
+                "type": result[2],
+                "enonce": result[3],
+                "demonstration": result[4],
+                "verification": result[5],
+                "date_modification": result[6],
+                "mathematicien": {"id": result[7], "mathematicien": result[8]}
+                if result[7] else None,
+                "categorie": {"id": result[9], "category": result[10]}
+                if result[9] else None,
+            }
+            # Récupérer les alias du concept
+            cursor.execute("SELECT alias FROM aliases WHERE concept_id = %s", (concept_id,))
+            concept["aliases"] = [row[0] for row in cursor.fetchall()]
+
+            # Récupérer les sources liées au concept
+            cursor.execute("""
+                           SELECT DISTINCT s.id, s.titre, s.auteur, s.annee, s.url, s."type"
+                           FROM sources s
+                                    JOIN concepts_sources cs ON s.id = cs.source_id
+                           WHERE cs.concept_id = %s
+                           """, (concept_id,))
+            concept["sources"] = [
+                {
+                    "id": row[0],
+                    "titre": row[1],
+                    "auteur": row[2],
+                    "annee": row[3],
+                    "url": row[4],
+                    "type": row[5],
+                } for row in cursor.fetchall()
+            ]
+
+            # Récupérer les relations du concept (sources ou cibles)
+            cursor.execute("""
+                           SELECT r.id,
+                                  r.concept_source,
+                                  c_source.nom AS nom_source,
+                                  r.concept_cible,
+                                  c_cible.nom  AS nom_cible,
+                                  r.type_relation,
+                                  r.description,
+                                  r.date_relation
+                           FROM relations r
+                                    JOIN concepts c_source ON r.concept_source = c_source.id
+                                    JOIN concepts c_cible ON r.concept_cible = c_cible.id
+                           WHERE concept_source = %s
+                              OR concept_cible = %s
+                           """, (concept_id, concept_id))
+            concept["relations"] = [
+                {
+                    "id": row[0],
+                    "concept_source": {"id": row[1], "nom": row[2]},
+                    "concept_cible": {"id": row[3], "nom": row[4]},
+                    "type_relation": row[5],
+                    "description": row[6],
+                } for row in cursor.fetchall()
+            ]
+            cursor.execute("""
+                           SELECT id, "Nom_francais", "Nom_étranger", langue
+                           FROM foreign_name
+                           WHERE "Nom_francais" = (SELECT nom FROM concepts WHERE id = %s)
+
+                           """, (concept_id,))
+            concept["noms_etrangers"] = [
+                {
+                    "id": row[0],
+                    "Nom_francais": row[1],
+                    "Nom_étranger": row[2],
+                    "langue": row[3],
+                } for row in cursor.fetchall()
+            ]
+        if not concept:
+            raise HTTPException(status_code=404, detail="Concept non trouvé")
+        return concept
+
     @staticmethod
     def get_all_concepts_name()->list[ConceptName]:
         conn = get_db_connection()
